db3be56d4858a9af15c320f3742883f288a2c83a
ahmad_amirudin <4mirfor3v3r@gmail.com>
2020-08-05 15:16:55 +0700

    add sentry integration

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose')
 const bodyparser = require('body-parser');
 const url = 'mongodb://localhost:27017/dbJelajahRasa';
 const Index = require('./index')
+const Sentry = require('@sentry/node')
 
+app.use(Sentry.Handlers.requestHandler());
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({
     extended:false
@@ -19,6 +21,9 @@ mongoose
     .catch(e =>{
         console.log(e)
     })
+Sentry.init({ dsn: 'https://5bfa1787fdcb4c3eb06d9d9dca96583f@o422126.ingest.sentry.io/5378074' });
+
 app.use('/api.jelajahrasa.com/v1',Index)
+app.use(Sentry.Handlers.errorHandler());
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
  
